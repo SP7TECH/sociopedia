84f1cd8b62f22d177eece67221b07162a0e0165c
SP7TECH <spandey247789@gmail.com>
2023-04-03 22:42:18 +0530

    Completed the login flow

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -17,6 +17,12 @@ export const register = async (req, res) => {
       occupation,
     } = req.body;
 
+    const user = await client
+      .db("sociopedia")
+      .collection("users")
+      .findOne({ email: email });
+    if (user) return res.status(500).json({ error: "User already exists" });
+
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
@@ -41,4 +47,28 @@ export const register = async (req, res) => {
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
+};
+
+// Log In
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await client
+      .db("sociopedia")
+      .collection("users")
+      .findOne({ email: email });
+
+    if (!user) return res.status(400).json({ error: "User does not exist" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(403).json({ error: "Invalid credentials " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+
+    res.status(200).json({ token, user });
+  } catch (error) {}
 };
