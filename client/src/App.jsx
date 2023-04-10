f92f16a54780a966e71fd5a7cdf92dcb5d786fd8
SP7TECH <spandey247789@gmail.com>
2023-04-10 23:59:52 +0530

    Theme Setup & Material UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,27 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import Profile from "./views/Profile";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
