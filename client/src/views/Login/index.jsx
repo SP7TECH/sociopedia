7953794c781d5414ca5cc7af72891e3c4e86a954
SP7TECH <spandey247789@gmail.com>
2023-04-14 08:36:47 +0530

    Worked on Login & Register Form Validation

--- client/src/views/Login/index.jsx
+++ client/src/views/Login/index.jsx
@@ -1,8 +1,36 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import Form from "./../../components/Form";
+
 const Login = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 992px)");
+
   return (
-    <div>
-      <p>Login Page</p>
-    </div>
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        bgcolor={theme.palette.background.alt}
+        borderRadius="1.5rem"
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Sociopedia, the Social Media Platform for social creatures
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
   );
 };
 
